92cdd14169a1d62ebd36382f98d691ad1529548c
CheeekyKeeeks <97700596+CheeekyKeeeks@users.noreply.github.com>
2024-05-07 14:49:48 -0400

    Add files via upload

---
+++ Exercise 8.2.py
@@ -0,0 +1,15 @@
+# Positive Indices
+
+>>> fruit = 'banana'
+>>> length = len(fruit)
+>>> last = fruit[length-1]
+>>> print(last)
+a
+
+# Negative Indices 
+
+>>> fruit = 'banana'
+>>> length = len(fruit)
+>>> last = fruit[length-1]
+>>> print(last)
+a
---
+++ Exercise 8.3.py
@@ -0,0 +1,24 @@
+Forward For Loop
+
+>>> fruit = "peach"
+>>> for char in fruit:
+...     print(char)
+...
+p
+e
+a
+c
+h
+
+Reversed For Loop
+
+>>> fruit = 'peach'
+>>> reversed_fruit = fruit[::-1]
+>>> for char in reversed_fruit:
+...     print(char)
+...
+h
+c
+a
+e
+p
